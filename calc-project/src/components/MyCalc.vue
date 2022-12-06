35b4f44092543fb32a60af0b3fb29b91071156c3
martin870705 <a0976151179@gmail.com>
2022-12-06 13:10:15 +0800

    calcCli

---
+++ calc-project/src/components/MyCalc.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <template v-for="(item, index) in btn" :key="index">
+      <button @click="operation(item)" :value="item">{{ item }}</button>
+    </template>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        btn:['+', '-', '*', '/', 'AC'],
+      };
+    },
+    methods: {  
+      operation(item){
+        this.$emit('operation', item)
+      }
+    },
+  }
+</script>
+
+<style>
+  body{
+    background-color: rgb(209, 179, 143);
+  }
+  button{
+    margin: 10px 0;
+    padding: 7px 15px;
+    border: 0;
+    box-shadow: 2px 2px 2px rgb(161, 161, 161);
+    background-color:gray ;
+    transform: scale(1);
+    transition: .1s;
+  }
+  button:hover{
+    background-color: rgb(146, 145, 145);
+    transform: scale(1.05);
+  }
+  button:active{
+    box-shadow: none;
+    transform: scale(1);
+  }
+  button + button{
+    margin-left: 15px;
+  }
+</style>
---
+++ calc-project/src/views/CalcView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <label for="num1">數字一: </label>
+    <input type="number" id="num1" v-model="num1">
+  </div>
+  <div>
+    <label for="num2">數字二: </label>
+    <input type="number" id="num2" v-model="num2">
+  </div>
+  <my-calc @operation="opera"></my-calc>
+  <div class="ans">
+    {{ ans }}
+  </div>
+</template>
+
+<script>
+  import MyCalc from '@/components/MyCalc.vue'
+  export default{
+    components: {
+      MyCalc,
+    },
+    data() {
+      return {
+        num1: '',
+        num2: '',
+        ans: '0',
+      };
+    },
+    methods: {
+      opera(val) {
+        // console.log(typeof this.num1, typeof this.num2);
+        // console.log(val);
+        if(val == 'AC'){
+          this.ans = '0';
+        }else if(this.num1 == '' && this.num2 == ''){
+          return;
+        }else if(val == '+'){
+          this.ans = this.num1 + this.num2;
+        }else if(val == '-'){
+          this.ans = this.num1 - this.num2;
+        }else if(val == '*'){
+          this.ans = this.num1 * this.num2;
+        }else{
+          this.ans = this.num1 / this.num2;
+        }
+        
+        this.num1 = '';
+        this.num2 = '';
+      }
+    },
+  }
+</script>
+
+<style>
+  div input{
+    border: none;
+    line-height: 20px;
+    margin-bottom: 5px;
+  }
+  .ans{
+    font-weight: bold;
+    font-size: 30px;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
+</style>
